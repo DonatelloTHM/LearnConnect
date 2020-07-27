75f8f18792bfbf1bd85c13e566ad7803b7dadd1c
“DonatelloTHM” <donat.pllana@outlook.com>
2020-07-27 18:34:53 -0400

    Login fixed

--- app/views/user_sessions/new.html.rb
+++
@@ -1,8 +0,0 @@
-
-
-<%= form_tag login_tutor_path, method: :post do%>
-    <%= label_tag :username %><br>
-    <%= text_field_tag :name %>
-    <%= password_field_tag :password %>
-    <%= submit_tag "Log in"%>
-<% end%>
