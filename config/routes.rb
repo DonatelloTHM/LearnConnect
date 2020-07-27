0946c70f6074a3d290edcaf49ebb6472e62e834d
“DonatelloTHM” <donat.pllana@outlook.com>
2020-07-27 17:19:42 -0400

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  resources :tutoring_sessions
-  resources :event_tutors
-  resources :events
   resources :tutors
-  resources :cities
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
